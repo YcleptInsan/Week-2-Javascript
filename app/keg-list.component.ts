e97187ddf8c278b3d0039bdff350c824fa67abdc
guy anderson and nick wise <guy+nick@>
2017-07-06 16:28:00 -0700

    use 2 pipes to sort by abv, price, and pint level.

--- app/keg-list.component.ts
+++ app/keg-list.component.ts
@@ -4,8 +4,22 @@ import { Keg } from './keg.model';
 @Component({
   selector: 'keg-list',
   template: `
+
+<select (change)="onchange($event.target.value)">
+  <option value="allKegs">All Kegs</option>
+  <option value="lessThanTarget">Low Pint level</option>
+</select>
+<br>
+<br>
+<select (change)="onSort($event.target.value)">
+  <option value="priceSort">Sort by Price</option>
+  <option value="abvSort">Sort by Abv</option>
+</select>
+<br>
+<br>
+
   <ul>
-    <li *ngFor="let currentKeg of childKegList"><strong>Name:</strong> {{currentKeg.name}}<br><strong>Price:</strong> {{currentKeg.price}}<br><strong>abv: </strong>{{currentKeg.abv}}<br><strong>Pint Remaining: </strong>{{currentKeg.pint}}<br>
+    <li *ngFor="let currentKeg of childKegList | kegLow:filterByLowLevel | abvSort:filterByOptions"><strong>Name:</strong> {{currentKeg.name}}<br><strong>Price:</strong> {{currentKeg.price}}<br><strong>abv: </strong>{{currentKeg.abv}}<br><strong>Pint Remaining: </strong>{{currentKeg.pint}}<br>
     <button (click)="editButtonHasBeenClicked(currentKeg)"> Edit </button>
     <button (click)="serveButton(currentKeg)">Serve Pint</button>
     <br><br>
@@ -18,6 +32,18 @@ export class KegListComponent {
   @Input() childKegList: Keg[];
   @Output() clickSender = new EventEmitter;
   @Output() clickSenderServe = new EventEmitter;
+
+  filterByLowLevel: string ="allKegs";
+
+  filterByOptions: string ="";
+
+  onSort(optionFromMenu) {
+    this.filterByOptions = optionFromMenu;
+  }
+
+  onchange(optionFromMenu) {
+    this.filterByLowLevel = optionFromMenu;
+  }
 
   editButtonHasBeenClicked(kegToEdit: Keg) {
     this.clickSender.emit(kegToEdit);
