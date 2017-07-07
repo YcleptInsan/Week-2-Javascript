1e3ba8875a098972eb3033d4e9957b39e066a983
nick wise <nickmwise@gmail.com>
2017-07-07 08:32:24 -0700

    add root component with boiler plate information. Welcome page and a master list of animals I have already made.

---
+++ app/app.component.ts
@@ -0,0 +1,22 @@
+import { Component } from '@angular/core';
+import { Animal  } from './animal.model';
+
+@Component({
+  selector: 'app-root',
+  template: `
+  <div class="container">
+    <h1>Welcome to the Zoo Portal</h1>
+
+  </div>
+  `
+})
+
+export class AppComponent {
+  selectedAnimal = null;
+
+  masterAnimalList: Animal[] = [
+    new Animal("Artic Fox", "Jessie", 5 , "Carnivore", "North Pole", 2, "Female", "Cold Weather", "Humans"),
+    new Animal("Black Bear", "James", 1 ,"Carnivore", "Western Hemisphere", 3, "Male", "Bear-ies", "Loud noises"),
+    new Animal("Pacific Walrus", "Fred", 12 , "Carnivore", "North Pole", 8, "Male", "Cold Water", "Killer Whales"),
+  ];
+}
